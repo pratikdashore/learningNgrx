dc8c18f3d1ec70d50fe2e937d4945815c1ed36d2
Pratik Dashore <pratik.dashore@accolitelabs.com>
2018-09-25 22:32:03 +0530

    Creating some more actions

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -5,7 +5,11 @@ export enum ProductActionTypes {
   ToggleProductCode = "[Product] Toggle Product Code",
   SetCurrentProduct = "[Product] Set Current Product",
   ClearCurrentProduct = "[Product] Clear Current Product",
-  InitializeCurrentProduct = "[Product] Initialize Current Product"
+  InitializeCurrentProduct = "[Product] Initialize Current Product",
+
+  LoadProducts = "[Product] Load products",
+  LoadProductsSuccess = "[Product] Load Product Success",
+  LoadProductsFail = "[Product] Load Product Fail"
 }
 
 export class ToggleProductCode implements Action {
@@ -26,8 +30,25 @@ export class ClearCurrentProduct implements Action {
   readonly type = ProductActionTypes.ClearCurrentProduct;
 }
 
+export class LoadProducts implements Action {
+  readonly type = ProductActionTypes.LoadProducts;
+}
+
+export class LoadProductsSuccess implements Action {
+  readonly type = ProductActionTypes.LoadProductsSuccess;
+  constructor(public payload: Product[]) {}
+}
+
+export class LoadProductsFail implements Action {
+  readonly type = ProductActionTypes.LoadProductsFail;
+  constructor(public payload: string) {}
+}
+
 export type ProductActions =
   | ToggleProductCode
   | SetCurrentProduct
   | InitializeCurrentProduct
-  | ClearCurrentProduct;
+  | ClearCurrentProduct
+  | LoadProducts
+  | LoadProductsSuccess
+  | LoadProductsFail;
