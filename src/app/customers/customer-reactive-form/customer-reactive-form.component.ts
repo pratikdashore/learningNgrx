cd17a363555362902d2ea96dc5ef4df75abcef2d
Pratik Dashore <pratik.dashore@accolitelabs.com>
2020-07-07 14:50:19 +0530

    basic reactive form

--- src/app/customers/customer-reactive-form/customer-reactive-form.component.ts
+++ src/app/customers/customer-reactive-form/customer-reactive-form.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Customer } from '../models/customer';
 
 @Component({
   selector: 'pm-customer-reactive-form',
   templateUrl: './customer-reactive-form.component.html',
-  styleUrls: ['./customer-reactive-form.component.css']
+  styleUrls: ['./customer-reactive-form.component.css'],
 })
 export class CustomerReactiveFormComponent implements OnInit {
+  customer: Customer = new Customer();
 
-  constructor() { }
+  customerForm: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+    });
   }
 
+  onSave() {
+    console.log(JSON.stringify(this.customerForm.value));
+  }
 }
