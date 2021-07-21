36f355bf4fab6c55df89938205859f333fdc30ed
Pratik Dashore <pratik.dashore@accolitelabs.com>
2021-07-21 18:37:08 +0530

    updated to 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
